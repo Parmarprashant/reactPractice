8ebca16c5c87f6eadba438ed1a64b055b4b3f324
Prashant Parmar <prashantparmar9919@gmail.com>
2026-02-05 14:46:00 +0530

    added react pracice Project

---
+++ Practice-io/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Counter from './components/Counter'
+import Toggle from './components/Toggle'
+import Submit from './components/Input'
+function App() {
+ 
+  return (
+    <div>
+      <h1>Prashant </h1>
+
+      <Counter></Counter>
+<br />
+      <Toggle></Toggle>
+
+      <br />
+      <br />
+
+      <Submit></Submit>
+    </div>
+  )
+}
+
+export default App

a6381eb6fd064c08d09a03e5497459e9d3399e85
Prashant Parmar <prashantparmar9919@gmail.com>
2026-02-11 00:27:11 +0530

    added singleState form

---
+++ single-useState/src/App.jsx
@@ -0,0 +1,58 @@
+import {useState} from 'react'
+
+function App() {
+
+  const [details, setDetails] = useState({
+    namedd: "",
+    emaill: "",
+    phonee: ""
+  })
+
+  function handleMain(e){
+        e.preventDefault();
+        console.log(details);
+        
+  }
+
+
+function handleSubmit(e){
+    const { name, value } = e.target;
+     setDetails(prev =>({
+      ...prev,
+      [name]: value
+      
+     }))
+}
+
+  return (
+    <>
+    <div className="main">
+    <form onSubmit={handleMain}>
+       
+       <label htmlFor="named">Name:- </label>
+       <input type="text" id='named' name='namedd' onChange={handleSubmit}  value={details.namedd}/>
+       <br />
+       <br />
+       <label htmlFor="email">Email:- </label>
+       <input type="text" id='email' name='emaill' onChange={handleSubmit} value={details.emaill}/>
+
+       <br />
+       <br />
+
+       <label htmlFor="Phone">Phone number:- </label>
+       <input type="text" id='Phone' name='phonee' onChange={handleSubmit} value={details.phonee}/>
+       <br />
+       <br />
+       <button type="submit">Submit</button>
+
+
+      
+
+    </form>
+
+    </div>
+    </>
+  )
+}
+
+export default App
